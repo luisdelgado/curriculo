be77118794f06051fe10b0d13cad92f4eeb88824
luisdelgado <luishdelgados10@gmail.com>
2021-07-23 23:13:31 -0300

    feat: melhorando dimensões dos cards

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const project = {
   title: "Saúde 10 Anos no Futuro",
   subtitle: "Onde estamos e onde podemos ir",
   description:
-    "Foi um trabalho de Foresight Thinking desenvolvido na disciplina Pensando Empreendimentos no Futuro em 2018. Coletar sinais, reunir tendências, fazer previsões, criar possibilidades de cenários, construir personas e narrativas futuras estão entre as...",
+    "Foi um trabalho de Foresight Thinking desenvolvido na disciplina Pensando Empreendimentos no Futuro em 2018. Coletar sinais, reunir tendências, fazer previsões, criar possibilidades de cenários, construir personas e...",
 };
 const project2 = {
   image: "assets/acidentesTransitoRecife.png",
@@ -17,7 +17,7 @@ const project3 = {
   title: "TCM Cariri Olindense",
   subtitle: "Landing page",
   description:
-    "Desde 2020 tenho a oportunidade de fazer a landing page da TCM Cariri Olindense. Conhecida como a mais antiga troça de frevo de Olinda, foi fundada em 15 de fevereiro de 1921. Trabalhei tanto no design, utilizando as melhores abordagens de Design Thinking, tanto no Desenvolvimento...",
+    "Conhecida como a mais antiga troça de frevo de Olinda, foi fundada em 15 de fevereiro de 1921. Desde 2020, trabalho tanto no design, utilizando as melhores abordagens de Design Thinking, tanto no desenvolvimento Web Front-end com Angular 8.",
 };
 const projectsDatabase = [project, project2, project3];
 var projectsHTML = document.getElementById("project");
@@ -28,11 +28,12 @@ projectsDatabase.forEach((project) => {
         <img src="${project.image}" alt="${project.title}" style="width:100%">
         <div class="block__container">
             <h4><b>${project.title}</b></h4> 
-            <h5><b>${project.subtitle ? project.subtitle : ""}</b></h5> 
+            ${project.subtitle ? `<h5><b>${project.subtitle}</b></h5>` : ""} 
             <p>${project.description}</p> 
         </div>
     </div>
 </div>`;
 });
 
-//
+// Hobbies
+// Filmes
